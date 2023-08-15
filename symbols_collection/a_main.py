2022fef17ad9df7bf730b7ee8f74939da3d4d6d6
75959523 <54936383+75959523@users.noreply.github.com>
2023-08-15 18:34:43 +0800

    commit

--- symbols_collection/a_main.py
+++ symbols_collection/a_main.py
@@ -1,14 +1,26 @@
+from symbols_collection.ascend_ex import ascend_ex
 from symbols_collection.bi_ka import bi_ka
+from symbols_collection.bigone import bigone
+from symbols_collection.binance import binance
 from symbols_collection.bit_get import bit_get
+from symbols_collection.bit_mart import bit_mart
 from symbols_collection.bit_venus import bit_venus
 from symbols_collection.bitfinex import bitfinex
+from symbols_collection.bybit import bybit
+from symbols_collection.coin_tiger import coin_tiger
 from symbols_collection.coin_w import coin_w
 from symbols_collection.deep_coin import deep_coin
+from symbols_collection.digi_finex import digi_finex
 from symbols_collection.gate_io import gate_io
+from symbols_collection.hitbtc import hitbtc
 from symbols_collection.hot_coin import hot_coin
 from symbols_collection.huobi import huobi
+from symbols_collection.jubi import jubi
+from symbols_collection.l_bank import l_bank
 from symbols_collection.mexc import mexc
 from symbols_collection.okx import okx
+from symbols_collection.poloniex import poloniex
+from symbols_collection.xt import xt
 
 okx()
 huobi()
@@ -21,3 +33,15 @@ bit_venus()
 bi_ka()
 deep_coin()
 hot_coin()
+ascend_ex()
+poloniex()
+bybit()
+xt()
+hitbtc()
+digi_finex()
+bit_mart()
+bigone()
+coin_tiger()
+l_bank()
+jubi()
+binance()
