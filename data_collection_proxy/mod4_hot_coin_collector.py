62875587b6422273154ce3228d9bef1b2f6863cc
75959523 <54936383+75959523@users.noreply.github.com>
2023-08-27 21:33:12 +0800

    commit

---
+++ data_collection_proxy/mod4_hot_coin_collector.py
@@ -0,0 +1,65 @@
+import asyncio
+import time
+import httpx
+
+from config.logger_config import setup_logger
+from data_processing_proxy.mod4_hot_coin_processor import filter_symbols, insert_to_db
+from proxy_handler.proxy_loader import ProxyRotator
+
+rotator = ProxyRotator()
+logger = setup_logger("hot_coin_collector", "log/app.log")
+max_concurrent_requests = 500
+retry_limit = 3
+
+
+def hot_coin(symbols, temp_table_name):
+    start_time = time.time()
+    data = asyncio.run(hot_coin_symbols())
+    if data:
+        found_records = filter_symbols(symbols, data)
+        result = asyncio.run(hot_coin_depth(found_records))
+        insert_to_db(result, temp_table_name)
+    else:
+        logger.error("Failed to get tickers from hot_coin")
+
+    end_time = time.time()
+    elapsed_time = round(end_time - start_time, 3)
+    logger.info(f"-------------------------------------------------- hot_coin executed in {elapsed_time} seconds.")
+
+
+async def hot_coin_symbols():
+    proxy = rotator.get_next_proxy()
+    url = "https://api.hotcoinfin.com/v1/market/ticker"
+    async with httpx.AsyncClient(proxies=proxy, verify=False, timeout=10) as client:
+        response = await client.get(url)
+        return response.json()['ticker'] if response.status_code == 200 else None
+
+
+async def hot_coin_depth(found_records):
+    url = "https://api.hotcoinfin.com/v1/depth?symbol="
+    semaphore = asyncio.Semaphore(max_concurrent_requests)
+    tasks = [fetch(symbol, url, semaphore) for symbol in found_records]
+    results = await asyncio.gather(*tasks)
+    return {symbol: result for symbol, result in results if result}
+
+
+async def fetch(symbol, url, semaphore):
+    proxy = rotator.get_next_proxy()
+
+    for retry in range(retry_limit):
+        async with semaphore:
+            try:
+                async with httpx.AsyncClient(proxies=proxy, verify=False, timeout=20) as client:
+                    response = await client.get(url + symbol)
+                    if response.status_code == 200:
+                        # logger.info("success: " + symbol)
+                        return symbol, response.json()
+                    else:
+                        logger.info(f"Request failed {symbol} status code {response.status_code} - hot_coin")
+                        await asyncio.sleep(0.1)
+
+            except Exception as e:
+                logger.error(f"Error fetching {symbol}. Reason: {repr(e)} - hot_coin")
+                await asyncio.sleep(0.1)
+
+    return symbol, None
